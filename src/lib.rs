76c0b7e93c6b6f159b20a3cb350ff114fe53b38a
Kato Masato <masato.kato0604@gmail.com>
2023-05-06 20:35:44 +0900

    remove unnecessary methods

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,3 @@ mod prf;
 pub mod handshake;
 mod enums;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
